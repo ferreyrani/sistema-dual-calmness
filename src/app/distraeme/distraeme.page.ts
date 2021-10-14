d898b7cd11ba6a45730320fb23abe9b1add15719
DianaCaro <dianacaromuller@gmail.com>
2021-10-14 12:21:11 -0300

    Diseño: Distraeme

--- src/app/distraeme/distraeme.page.ts
+++ src/app/distraeme/distraeme.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-distraeme',
   templateUrl: './distraeme.page.html',
   styleUrls: ['./distraeme.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DistraemePage implements OnInit {
 
