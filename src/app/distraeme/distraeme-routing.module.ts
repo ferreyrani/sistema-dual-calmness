11a83dda342dcae26d2d9951a6980161d6d613e7
DianaCaro <dianacaromuller@gmail.com>
2021-10-14 10:43:42 -0300

    Creación: Juegos, Videos y Playlist

--- src/app/distraeme/distraeme-routing.module.ts
+++ src/app/distraeme/distraeme-routing.module.ts
@@ -7,6 +7,18 @@ const routes: Routes = [
   {
     path: '',
     component: DistraemePage
+  },
+  {
+    path: 'juegos',
+    loadChildren: () => import('./juegos/juegos.module').then( m => m.JuegosPageModule)
+  },
+  {
+    path: 'playlist',
+    loadChildren: () => import('./playlist/playlist.module').then( m => m.PlaylistPageModule)
+  },
+  {
+    path: 'videos',
+    loadChildren: () => import('./videos/videos.module').then( m => m.VideosPageModule)
   }
 ];
 
