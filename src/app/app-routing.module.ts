8d4443043262cd7e5165642bb5e10d2e01f5ee6b
guillehimmel <92099275+guillehimmel@users.noreply.github.com>
2021-10-17 12:49:44 -0300

    paginfo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -107,7 +107,11 @@ const routes: Routes = [
   {
     path: 'minecraft',
     loadChildren: () => import('./juegos/minecraft/minecraft.module').then( m => m.MinecraftPageModule)
+  },  {
+    path: 'informacion',
+    loadChildren: () => import('./informacion/informacion.module').then( m => m.InformacionPageModule)
   },
+
 
 ];
 
---
+++ src/app/informacion/informacion.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-informacion',
+  templateUrl: './informacion.page.html',
+  styleUrls: ['./informacion.page.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class InformacionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
