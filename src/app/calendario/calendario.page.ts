d8227d0aa2edf862f5f19e1f8b11ce9db33c636b
DianaCaro <dianacaromuller@gmail.com>
2021-10-19 13:11:25 -0300

    Creacion de paginas de MENU y Agenda

--- src/app/calendario/calendario.page.ts
+++ src/app/calendario/calendario.page.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  ViewEncapsulation  } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.page.html',
   styleUrls: ['./calendario.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CalendarioPage implements OnInit {
-
-  constructor() { }
+  taskName = "";
+  taskList = [];
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  addTask() {
+    if (this.taskName.length > 0) {
+        let task = this.taskName;
+        this.taskList.push(task);
+        this.taskName = "";
+    }
 }
+deleteTask(index){
+  this.taskList.splice(index, 1);
+}
+async updateTask(index) {
+  const alert = await this.alertCtrl.create({
+    cssClass: 'my-custom-class',
+    header: 'Update Task?',
+    message: 'Type in your new task to update.',
+    inputs: [{ name: 'editTask', placeholder: 'Task' }],
+      buttons: [{ text: 'Cancel', role: 'cancel' },
+                { text: 'Update', handler: data => {
+                    this.taskList[index] = data.editTask; }
+                }]
+  });
+
+  await alert.present();
+
+  const { role } = await alert.onDidDismiss();
+  console.log('onDidDismiss resolved with role', role);
+}
+
+}
+
--- src/app/soporte/soporte.page.ts
+++ src/app/soporte/soporte.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-soporte',
   templateUrl: './soporte.page.html',
   styleUrls: ['./soporte.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SoportePage implements OnInit {
 
