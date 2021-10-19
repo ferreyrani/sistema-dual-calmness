d92cd73884500490133c58c65771837e0e57e511
guillehimmel <92099275+guillehimmel@users.noreply.github.com>
2021-10-19 13:01:23 -0300

    Perfil

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PerfilPage implements OnInit {
 
