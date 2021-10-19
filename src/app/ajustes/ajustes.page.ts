f2faa3ccd8c1a959ff06ed8ed0fff8d198290992
Zoe-Br <92101418+Zoe-Br@users.noreply.github.com>
2021-10-19 12:38:13 -0300

    Ajustes

--- src/app/ajustes/ajustes.page.ts
+++ src/app/ajustes/ajustes.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-ajustes',
   templateUrl: './ajustes.page.html',
   styleUrls: ['./ajustes.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AjustesPage implements OnInit {
 
